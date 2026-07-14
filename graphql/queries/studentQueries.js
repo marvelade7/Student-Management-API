0e06380163b5e10c9161b540082a3127e45989cd
Adewuyi Marvellous <marveladeadewuyi@gmail.com>
2026-07-14 13:33:46 +0100

    feat: enhance student queries with filtering options for level, gender, department, and faculty; implement search functionality by keyword

--- graphql/queries/studentQueries.js
+++ graphql/queries/studentQueries.js
@@ -3,7 +3,7 @@ const Student = require("../../models/student.model");
 const StudentType = require("../types/studentType");
 const StudentPaginationType = require("../types/studentPagination");
 
-const { GraphQLID, GraphQLList, GraphQLInt } = graphql;
+const { GraphQLID, GraphQLList, GraphQLInt, GraphQLString } = graphql;
 
 module.exports = {
     student: {
@@ -27,17 +27,131 @@ module.exports = {
             limit: {
                 type: GraphQLInt,
             },
+            level: {
+                type: GraphQLString,
+            },
+            gender: {
+                type: GraphQLString,
+            },
+            department: {
+                type: GraphQLID,
+            },
+            faculty: {
+                type: GraphQLID,
+            },
         },
         resolve(parent, args) {
+            const filter = {};
+            if (args.level) {
+                filter.level = args.level;
+            }
+            if (args.gender) {
+                filter.gender = args.gender;
+            }
+            if (args.department) {
+                filter.department = args.department;
+            }
+            if (args.faculty) {
+                filter.faculty = args.faculty;
+            }
             const page = args.page || 1;
             const limit = args.limit || 10;
 
             const skip = (page - 1) * limit;
 
-            return Student.countDocuments().then((totalStudents) => {
-                return Student.find()
+            return Student.countDocuments(filter).then((totalStudents) => {
+                return Student.find(filter)
                     .skip(skip)
                     .limit(limit)
+                    .then((students) => {
+                        const totalPages = Math.ceil(totalStudents / limit);
+
+                        return {
+                            students,
+                            currentPage: page,
+                            totalPages,
+                            totalStudents,
+                            hasNextPage: page < totalPages,
+                            hasPreviousPage: page > 1,
+                        };
+                    });
+            });
+        },
+    },
+
+    searchStudent: {
+        type: StudentPaginationType,
+        args: {
+            keyword: {
+                type: GraphQLString,
+            },
+            page: {
+                type: GraphQLInt,
+            },
+            level: {
+                type: GraphQLInt,
+            },
+            gender: {
+                type: GraphQLString,
+            },
+            department: {
+                type: GraphQLID,
+            },
+            limit: {
+                type: GraphQLInt,
+            },
+        },
+        resolve(parent, args) {
+            const filter = {};
+            if (args.level) {
+                filter.level = args.level;
+            }
+            if (args.gender) {
+                filter.gender = args.gender;
+            }
+            if (args.department) {
+                filter.department = args.department;
+            }
+
+            if (args.keyword) {
+                filter.$or = [
+                    {
+                        firstName: {
+                            $regex: args.keyword,
+                            $options: "i",
+                        },
+                    },
+                    {
+                        lastName: {
+                            $regex: args.keyword,
+                            $options: "i",
+                        },
+                    },
+                    {
+                        email: {
+                            $regex: args.keyword,
+                            $options: "i",
+                        },
+                    },
+                    {
+                        matricNumber: {
+                            $regex: args.keyword,
+                            $options: "i",
+                        },
+                    },
+                ];
+            }
+
+            const page = args.page || 1;
+            const limit = args.limit || 10;
+
+            const skip = (page - 1) * limit;
+
+            return Student.countDocuments(filter).then((totalStudents) => {
+                return Student.find(filter)
+                    .skip(skip)
+                    .limit(limit)
+                    .populate("department")
                     .then((students) => {
                         const totalPages = Math.ceil(totalStudents / limit);
 
